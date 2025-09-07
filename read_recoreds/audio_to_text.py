76ad397f783df7f47a869b5ea61ef2618b8ebd72
shuki <shukiheftler@gmail.com>
2025-09-07 11:30:13 +0300

    audio to text class created

--- read_recoreds/audio_to_text.py
+++ read_recoreds/audio_to_text.py
@@ -1,30 +1,21 @@
-from faster_whisper import WhisperModel
+import whisper
+import os
 
 
-
-
-
-
-
-
-
-
-# with open("transcription.txt", "w", encoding="utf-8") as f:
-#     for seg in segments:
-#         line = f"[{seg.start:.2f}s -> {seg.end:.2f}s] {seg.text}\n"
-#         f.write(line)
-#         print(line, end="")
 
 class AudioToText:
     def __init__(self,path):
-        self.model = WhisperModel("base", device="cpu", compute_type="int8")
+        os.environ["PATH"] += os.pathsep + r"C:\ffmpeg-8.0-essentials_build\bin"
         self.path = path
     def convert_audio(self):
-        segments, info = self.model.transcribe(self.path, beam_size=5)
-        print("segment", segments)
-        print("info", info)
-        print("Language detected:", info.language)
+        model = whisper.load_model("small")
+        result = model.transcribe(self.path)
+        return result["text"]
+
 
 if __name__ == '__main__':
-    att = AudioToText("podcasts/download.wav")
-    att.convert_audio()
+    att = AudioToText("../podcasts/download.wav")
+    print(att.convert_audio())
+
+
+
