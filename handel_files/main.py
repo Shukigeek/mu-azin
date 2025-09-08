982d665eec4a89983dcbfd7a8f902c56a2974618
shuki <shukiheftler@gmail.com>
2025-09-08 12:16:50 +0300

    log added to all the project

--- handel_files/main.py
+++ handel_files/main.py
@@ -1,5 +1,10 @@
 from handel_files.sand_to_kafka import Manager
-
+from logger.logger import Logger
+logger = Logger.get_logger()
 # sanding data to kafka
+
+logger.info("creating a instants of handel_files.sand_to_kafka => Manager")
 m = Manager()
+logger.info("producing data to kafka by topic from main")
 m.produce_to_kafka()
+logger.info("data produce successfully")
